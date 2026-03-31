04a7461f37044392cccd3b967d1c59fe570a77e9
anudeep-bonagiri <anudeep.bonagiri@gmail.com>
2026-03-31 00:52:34 -0500

    fix: switch to BrowserRouter and update Design Studio route to /uiinterns

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import V2 from "./pages/V2";
 import UIInterns from "./pages/UIInterns";
 import NotFound from "./pages/NotFound";
@@ -14,14 +14,14 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<V2 />} />
-          <Route path="/interns/ui-design" element={<UIInterns />} />
+          <Route path="/uiinterns" element={<UIInterns />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
